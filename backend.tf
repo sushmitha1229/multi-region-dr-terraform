e374bc7ea90bf2d14ec8b6e2f65df8a78e719410
sushmitha1229 <sushmithakeddy@gmail.com>
2025-09-25 14:02:52 -0400

    remote state (recommended: S3 + DynamoDB for locking)

--- backend.tf
+++ backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state"
+    key            = "multi-region/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
 
